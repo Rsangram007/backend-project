575a7cdd6a1586335beb4df39c88eaaec43cb05e
Rsangram007 <sangrambeheraa007@gmail.com>
2022-08-29 21:22:35 +0530

    ok

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
+     firstName: String,
+    // lastName: String,
+    // mobile: {
+    //     type: String,
 
-        required: true
-    },
-    emailId: String,
-    password: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "other"]
-    },
-    age: Number,
-    posts: {type: [], deafult: []}
-}, { timestamps: true });
+    //     required: true
+    // },
+    // emailId: String,
+    // password: String,
+    // gender: {
+    //     type: String,
+    //     enum: ["male", "female", "other"]
+    // },
+    // age: Number,
+   
+}, { timeStamps: true });
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('Name', userSchema)
